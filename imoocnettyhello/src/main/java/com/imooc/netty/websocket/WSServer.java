26d8d0d9a275f15eea22fa87f226c0e7d1e5a32b
1256233771 <1256233771@qq.com>
2019-12-30 23:45:36 +0800

    WS初始化类设置

---
+++ imoocnettyhello/src/main/java/com/imooc/netty/websocket/WSServer.java
@@ -0,0 +1,32 @@
+package com.imooc.netty.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @Author: clf
+ * @Date: 19-12-30
+ * @Description:
+ */
+public class WSServer {
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup subGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(mainGroup, subGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(null);
+            ChannelFuture future = server.bind(8080).sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            mainGroup.shutdownGracefully();
+            subGroup.shutdownGracefully();
+        }
+
+    }
+}
